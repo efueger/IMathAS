8e58c8acd9f9f42494d118195230d6e8244def71
Bracken Mosbacker <bracken@lumenlearning.com>
2015-04-13 09:08:52 -0600

    make the config work for local and AWS deploy

--- config/db.php
+++ config/db.php
@@ -1,9 +1,25 @@
 <?php
+if ($_SERVER['RDS_HOSTNAME'] != '' ) {
+  $dbhost = $_SERVER['RDS_HOSTNAME'];
+  $dbport = $_SERVER['RDS_PORT'];
+  $dbname = $_SERVER['RDS_DB_NAME'];
+
+  $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
+  $username = $_SERVER['RDS_USERNAME'];
+  $password = $_SERVER['RDS_PASSWORD'];
+} else {
+  $dbhost = 'localhost';
+  $dbname = 'imathasdb';
+
+  $dsn = "mysql:host={$dbhost};dbname={$dbname}";
+  $username = 'root';
+  $password = 'root';
+}
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=imathasdb',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => $dsn,
+    'username' => $username,
+    'password' => $password,
     'charset' => 'utf8',
 ];
